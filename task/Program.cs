0130ff1e9cd0b0fccb65c3a862d4bf1206c0d1db
Vitalii Baliuk <ipz221_bvv@student.ztu.edu.ua>
2024-02-11 10:13:58 +0200

    task1

---
+++ task/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n1, n2;
+        Console.WriteLine("Write 1st number");
+        int.TryParse(Console.ReadLine(), out n1);
+        Console.WriteLine("Write 2nd number");
+        int.TryParse(Console.ReadLine(), out n2);
+        int sum = n1 + n2;
+        Console.WriteLine($" {n1} + {n2} = {sum}");
+    }
+}
